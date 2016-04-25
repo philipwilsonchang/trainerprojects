6d4ab79475995fd07cc5aeb72cc2aefa3d060581
philipwilsonchang <philip.wilson.chang@gmail.com>
2016-04-25 11:30:15 -0400

    Implemented file I/O in randomGenerator

--- 03a randomGenerator.rb
+++ 03a randomGenerator.rb
@@ -1,7 +1,16 @@
 ### HELPER FUNCTIONS ###
 
 # Takes a length, list min, and list max and generates a list of randomly generated numbers from min to max of length
+# Throws error list if length < 0 or min > max or if length is not an integer value
 def generateRandomList(length, min, max)
+	unless length.is_a?(Integer)
+		return ["error"]
+	end
+	if length <= 0
+		return ["error"]
+	elsif min > max
+		return ["error"]
+	end
 	randomList = Array.new(length)
 	0.upto(length - 1) do |i|
 		randomList[i] = Random.rand(max - min) + min
@@ -20,4 +29,14 @@ print "List max? "
 listMax = gets.to_i()
 
 myList = generateRandomList(listLength, listMin, listMax)
-print myList
+if myList[0] == "error"
+	print "List error"
+else
+	print myList
+end
+
+fileWriter = File.new("randomDigitList.txt", "w+")
+0.upto(myList.length) do |i|
+	fileWriter.puts(myList[i])
+end
+fileWriter.close
