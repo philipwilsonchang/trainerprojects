0a8c5a1eaf308a433580d4b3c100896476d6d5cd
philipwilsonchang <philip.wilson.chang@gmail.com>
2016-04-26 11:01:43 -0400

    Implemented insertion sort with timer

--- 03b sortingAlgorithms.rb
+++ 03b sortingAlgorithms.rb
@@ -4,14 +4,32 @@
 def fileReader(filePath)
 	outList = Array.new()
 	File.open(filePath).readlines.each do |line|
-		outList.push(line.strip)
+		outList.push(line.strip.to_i())
 	end
+	outList.pop										# Remove empty last entry
 	return outList
 end
 
 # Takes in list and sorts contents using insertion sort method. Outputs list
 def insertionSort(list)
-
+	totalSize = list.length
+	numSorted = 0
+	while numSorted < totalSize - 1					# Repeat until sorted sublist is the same length as whole list
+		toBeSorted = list[numSorted + 1]			# Grab next unsorted element in list
+		0.upto(numSorted) do |i|					# Compare to each element in sorted list, starting from bottom
+			if list[i] > toBeSorted					# If element compared is bigger, put toBeSorted in index right below it
+				moveItem = list[numSorted + 1]
+				list.delete_at(numSorted + 1)
+				list.insert(i, moveItem)
+				numSorted += 1						# Increase recorded number of sorted items
+				break								# Break the for loop
+			end
+			if i == numSorted						# If toBeSorted is bigger than all values in sorted list
+				numSorted += 1						# Move on to next unsorted item
+			end
+		end
+	end
+	return list
 end
 
 # Takes in list and sorts contents using selection sort method. Outputs list
@@ -39,4 +57,7 @@ end
 path = "randomDigitList.txt"
 myList = fileReader(path)
 
-
+time1 = Time.now.to_f
+insertionSort(myList)
+time2 = Time.now.to_f
+print "Time elapsed: ", time2 - time1, "\n"
