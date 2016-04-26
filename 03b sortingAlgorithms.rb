49dcb3654e8b661703be1831b468d4f74df005f7
philipwilsonchang <philip.wilson.chang@gmail.com>
2016-04-26 16:31:44 -0400

    Expanded randomDigitList to 10000 digits and implemented selection and bubble sorts

--- 03b sortingAlgorithms.rb
+++ 03b sortingAlgorithms.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 #### HELPER FUNCTIONS ####
 
 # Takes filePath and outputs file into list, separated by newlines
@@ -34,12 +36,44 @@ end
 
 # Takes in list and sorts contents using selection sort method. Outputs list
 def selectionSort(list)
-	
+	totalSize = list.length
+	numSorted = 0
+	while numSorted < totalSize - 1					# Repeated until sorted sublist is the same length as whole list
+		smallest = list[numSorted]					# Set "smallest element" as first element in unsorted sublist
+		smallestIndex = numSorted
+		numSorted.upto(totalSize - 1) do |i|			# Search through all unsorted for lowest number
+			if list[i] <= smallest 					# If a smaller number is found
+				smallest = list[i]					# Record it
+				smallestIndex = i
+			end
+		end
+		list.delete_at(smallestIndex)				# Move smallest entry found to top of sublist
+		list.insert(numSorted, smallest)
+		numSorted += 1								# Move on to next unsorted item
+	end
+	return list
 end
 
 # Takes in list and sorts contents using bubble sort method. Outputs list
 def bubbleSort(list)
-
+	totalSize = list.length
+	numSorted = 0
+	swapSpace = 0
+	while numSorted < totalSize - 1							# Repeat until sorted sublist is the same length as whole list
+		compareIndex1 = 0									# Prime the bubble swapping
+		compareIndex2 = 1
+		0.upto(totalSize - 1 - numSorted - 1) do |i|		# Compare each two number pairs up until sorted boundary
+			if list[compareIndex1] > list[compareIndex2]	# If two compared numbers should be swapped
+				swapSpace = list[compareIndex1]				# Swap them
+				list[compareIndex1] = list[compareIndex2]
+				list[compareIndex2] = swapSpace
+			end
+			compareIndex1 += 1
+			compareIndex2 += 1
+		end
+		numSorted += 1										# Add one more item to sorted boundary
+	end
+	return list
 end
 
 # Takes in list and sorts contents using merge sort method. Outputs list
@@ -57,7 +91,20 @@ end
 path = "randomDigitList.txt"
 myList = fileReader(path)
 
-time1 = Time.now.to_f
-insertionSort(myList)
-time2 = Time.now.to_f
-print "Time elapsed: ", time2 - time1, "\n"
+print "Insertion Sort Benchmark:\n"
+print Benchmark.measure{insertionSort(myList)}, "\n"
+print "Selection Sort Benchmark:\n"
+print Benchmark.measure{selectionSort(myList)}, "\n"
+print "Bubble Sort Benchmark:\n"
+print Benchmark.measure{bubbleSort(myList)}, "\n"
+
+
+
+
+
+
+
+
+
+
+
